5efed4950b59a6559b0b9a1283bab975f9f7e4db
dsmith1227 <david.j.smith1227@gmail.com>
2026-09-02 16:14:10 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -189,14 +189,10 @@ with tab4:
     )
 
     st.dataframe(
-        renewal_summary.style.format({
-            "Auto_Renew_Sales": "{:,0f}",
-            "Manual_Renew_Sales": "{:,0f}"
-        }),    
+        renewal_summary,
         use_container_width=True,
         hide_index=True
     )
-
 # --------------------------------------------------
 # Raw Data
 # --------------------------------------------------
